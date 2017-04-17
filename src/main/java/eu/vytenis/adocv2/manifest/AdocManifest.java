2b57afc5f0a520b50b4f0fc7f98b320ae8045aa3
Vytenis Bivainis <monrealis@gmail.com>
2017-04-18 00:26:28 +0300

    Replace variable with query

--- src/main/java/eu/vytenis/adocv2/manifest/AdocManifest.java
+++ src/main/java/eu/vytenis/adocv2/manifest/AdocManifest.java
@@ -1,5 +1,8 @@
 package eu.vytenis.adocv2.manifest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import eu.vytenis.adocv2.AdocMarshaller;
 import eu.vytenis.adocv2.content.AdocContent;
 import oasis.names.tc.opendocument.xmlns.manifest._1.FileEntry;
@@ -7,17 +10,25 @@ import oasis.names.tc.opendocument.xmlns.manifest._1.Manifest;
 
 public class AdocManifest implements AdocContent {
 	private final AdocMarshaller marshaller = new AdocMarshaller();
-	private final Manifest manifest = new Manifest();
+	private final List<FileEntry> fileEntries = new ArrayList<FileEntry>();
 
 	public void add(String path, String contentType) {
-		FileEntry fe = new FileEntry();
-		fe.setFullPath("/");
-		fe.setMediaType("application/vnd.etsi.asic-e+zip");
-		manifest.getFileEntry().add(fe);
+		FileEntry entry = new FileEntry();
+		entry.setFullPath("/");
+		entry.setMediaType("application/vnd.etsi.asic-e+zip");
+		fileEntries.add(entry);
+		// TODO 2017-04-18 add directories
 	}
 
 	@Override
 	public String getAsString() {
+		Manifest manifest = createManifest();
 		return marshaller.marshallToString(manifest);
+	}
+
+	private Manifest createManifest() {
+		Manifest manifest = new Manifest();
+		manifest.getFileEntry().addAll(fileEntries);
+		return manifest;
 	}
 }
