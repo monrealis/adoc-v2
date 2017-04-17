dbef333e0b2c67c0c58a7490461c537eff345478
Vytenis Bivainis <monrealis@gmail.com>
2017-04-18 01:16:11 +0300

    Simplify manifest testing

--- src/test/java/eu/vytenis/adocv2/AdocPackageManifestTest.java
+++ src/test/java/eu/vytenis/adocv2/AdocPackageManifestTest.java
@@ -1,13 +1,18 @@
 package eu.vytenis.adocv2;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertThat;
 
 import java.io.StringReader;
+import java.util.List;
 
 import javax.xml.bind.JAXB;
 
 import org.junit.Test;
 
+import com.google.common.base.Joiner;
+
+import eu.vytenis.adocv2.mapping.Mapper;
 import oasis.names.tc.opendocument.xmlns.manifest._1.FileEntry;
 import oasis.names.tc.opendocument.xmlns.manifest._1.Manifest;
 
@@ -16,15 +21,25 @@ public class AdocPackageManifestTest {
 
 	@Test
 	public void manifestIsAValidXml() {
-		Manifest manifest = getManifest();
-		FileEntry file = manifest.getFileEntry().get(0);
-		assertEquals("/", file.getFullPath());
-		assertEquals("application/vnd.etsi.asic-e+zip", file.getMediaType());
+		String manifest = getManifestAsString();
+		assertThat(manifest, containsString("/ application/vnd.etsi.asic-e+zip"));
+	}
+
+	private String getManifestAsString() {
+		List<String> lines = new ToString().fromList(getManifest().getFileEntry()).map();
+		return Joiner.on("\n").join(lines);
 	}
 
 	private Manifest getManifest() {
 		String xml = adoc.getFileAsText("META-INF/manifest.xml");
 		Manifest manifest = JAXB.unmarshal(new StringReader(xml), Manifest.class);
 		return manifest;
+	}
+
+	private class ToString extends Mapper<FileEntry, String> {
+		@Override
+		public String map() {
+			return String.format("%s %s", from.getFullPath(), from.getMediaType());
+		}
 	}
 }
