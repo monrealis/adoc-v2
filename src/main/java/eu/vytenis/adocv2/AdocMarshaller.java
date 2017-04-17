1f4930db78e15d418e14b2f7b6f2f866a9ce7371
Vytenis Bivainis <monrealis@gmail.com>
2017-04-18 00:49:59 +0300

    Add TODO

--- src/main/java/eu/vytenis/adocv2/AdocMarshaller.java
+++ src/main/java/eu/vytenis/adocv2/AdocMarshaller.java
@@ -5,6 +5,7 @@ import java.io.StringWriter;
 import javax.xml.bind.JAXB;
 
 public class AdocMarshaller {
+	// TODO 2017-04-18 cache contexts
 	public String marshallToString(Object jaxbObject) {
 		StringWriter w = new StringWriter();
 		JAXB.marshal(jaxbObject, w);
